539ef42644327a1b452083deadeb8886bc79f933
polojerry <polojerry4@gmail.com>
2020-02-01 12:44:26 +0300

    Added single converter for HeroPowerStat, to use in conversion while storing to Database with HeroesDatabase Test

---
+++ app/src/androidTest/java/com/polotechnologies/heroes/database/favouriteHero/HeroesDatabaseTest.kt
@@ -0,0 +1,62 @@
+package com.polotechnologies.heroes.database.favouriteHero
+
+import androidx.room.Room
+import androidx.test.platform.app.InstrumentationRegistry
+import com.polotechnologies.heroes.dataModels.heroSubModels.*
+import com.polotechnologies.heroes.database.HeroesDatabase
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Test
+import java.io.IOException
+
+class HeroesDatabaseTest {
+
+    lateinit var heroesDaoFavouriteHero: DaoFavouriteHero
+    lateinit var database : HeroesDatabase
+
+    @Before
+    fun createDatabase() {
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        database = Room.inMemoryDatabaseBuilder(context, HeroesDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+        heroesDaoFavouriteHero = database.daoFavouriteHero
+    }
+
+/*    *//*@After
+    @Throws(IOException::class)
+    fun closeDatabase() {
+        database.close()
+    }
+*/
+    @Test
+    @Throws(IOException::class)
+    fun insertFavouriteHero(){
+        val heroApperance  = HeroAppearance("","", listOf(), listOf(), "", "")
+        val heroBiography = HeroBiography("","",listOf(),"", "", "","")
+        val heroConnections = HeroConnections("", "")
+        val heroImage = HeroImage("")
+        val heroPowerStat = HeroPowerStats("","","","","","")
+        val heroWork = HeroWork("","")
+
+        val favouriteHero  = FavouriteHero(0L,
+            "",
+            heroPowerStat,
+            heroBiography,
+            heroApperance,
+            heroWork,
+            heroConnections,
+            heroImage)
+
+        heroesDaoFavouriteHero.insert(favouriteHero)
+
+        val selectedFavouriteHero = heroesDaoFavouriteHero.getLatest()
+
+        assertEquals(selectedFavouriteHero?.name, "")
+    }
+
+}
---
+++ app/src/main/java/com/polotechnologies/heroes/database/favouriteHero/PowerStatsConverter.kt
@@ -0,0 +1,57 @@
+package com.polotechnologies.heroes.database.favouriteHero
+
+import androidx.room.TypeConverter
+import com.polotechnologies.heroes.dataModels.heroSubModels.HeroPowerStats
+
+class PowerStatsConverter {
+
+    @TypeConverter
+    fun toString(heroPowerStats: HeroPowerStats?): String {
+        var powerStats = ""
+
+        if (heroPowerStats == null) {
+            return powerStats
+        } else {
+            powerStats = "${heroPowerStats.intelligence}," +
+                    "${heroPowerStats.strength}," +
+                    "${heroPowerStats.speed}," +
+                    "${heroPowerStats.durability}," +
+                    "${heroPowerStats.power}," +
+                    "${heroPowerStats.combat},"
+        }
+
+
+        return powerStats
+
+    }
+
+    @TypeConverter
+    fun toHeroPowerStats(powerStats: String?): HeroPowerStats? {
+
+        var heroPowerStats: HeroPowerStats? = HeroPowerStats(
+            "", "", "", "", "", ""
+        )
+
+        if (powerStats.equals("")) {
+
+            return heroPowerStats
+
+        } else {
+
+            val stats: List<String> = powerStats!!.split(",")
+
+            heroPowerStats = HeroPowerStats(
+                stats[0],
+                stats[1],
+                stats[2],
+                stats[3],
+                stats[4],
+                stats[5]
+            )
+
+            return heroPowerStats
+        }
+
+
+    }
+}
